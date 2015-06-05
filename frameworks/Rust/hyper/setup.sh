00d2a56073450081c8bbddc26df9aaa6ebff11b3
Geoffroy Couprie <geo.couprie@gmail.com>
2015-06-05 12:39:38 +0200

    Update setup.sh

--- frameworks/Rust/hyper/setup.sh
+++ frameworks/Rust/hyper/setup.sh
@@ -3,8 +3,12 @@
 # Something like this will need to be done once we add database tests
 # sed -i 's|tcp(.*:3306)|tcp('"${DBHOST}"':3306)|g' src/main.rs
 
-CARGO=$IROOT/rust/bin/cargo
+CARGO=$IROOT/bin/cargo
+export PATH=$IROOT/bin:$PATH;
+export CARGO_HOME=$IROOT;
+export LD_LIBRARY_PATH=$IROOT/lib:$LD_LIBRARY_PATH;
 
 $CARGO build --release
 
 $CARGO run --release &
+
